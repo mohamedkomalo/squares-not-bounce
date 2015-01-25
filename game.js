59a6a03654b06c6a30101902a69f50a44fbe8ea1
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2015-01-25 14:00:19 +0200

    initial codebase

--- game.js
+++ game.js
@@ -7,211 +7,127 @@ function round(value, decimals) {
 }
 
 var GameController = (function(){
-	var GAME_SIZE = {width:700, height: 500};
-	var GAME_SCALE = 30;
-	var GAME_GRAVITY = 9.8;
-	var GAME_END = false;
 
 	var levels = [{
+		name: 'Little',
 		ball:{
-			pos:{},
+			x:0,
+			y:0,
 			radius:1,
-			color: 'black'
+			color: 'grey'
 		},
-		bricks:[{
-			pos:{},
-			size:{},
-			rotation:10
+		exit: {
+			type: 'vert',
+			x: 23,
+			y: 15
+		},
+		lifters:[{
+			x:0.5,
+			y:1,
+			width: 5,
+			height: 2,
+			incUp: 4,
+			incDown: 6
 		}]
 	}];
+
+	var GAME_SIZE = {width:700, height: 500};
+	var GAME_SCALE = 10;
+	var GAME_END = false;
 	
 	var nextLevelIndex = 0;
-	
-	var failTrials = 0;
+	var needRestartLevel = false;
+	var needMoveToNextLevel = false;
+
 	var startTime;
 	var endTime;
 
 	var canvas = document.getElementById('game');
-
 	canvas.width = GAME_SIZE.width;
 	canvas.height = GAME_SIZE.height;
 
 	var worldConfig = {
-		scale: GAME_SCALE,
-		tickFrequency: 50,
-		gravity: {x:0, y:GAME_GRAVITY}
+		scale: GAME_SCALE
 	};
+
 	var world = boxbox.createWorld(canvas, worldConfig);
 
-	var createBorders = function(){
-		world.camera({x:0, y:0});
-		// var boundaryTemplate = {
-		// 	name: 'boundary',
-		// 	shape: 'square',
-		// 	color: 'black',
-		// 	borderColor: 'black',
-		// 	type: 'static',
-		// 	width:GAME_SIZE.width / GAME_SCALE,
-		// 	height:GAME_SIZE.height / GAME_SCALE
-		// };
-
-		// var bottomBoundary = world.createEntity(boundaryTemplate, { height: 0.1, y: 0 + 0.1 });
-		// var leftBoundary = world.createEntity(boundaryTemplate, { width: 0.1, x: 0 + 0.1 });
-		// var rightBoundary = world.createEntity(boundaryTemplate, { width: 2, x: 70.5 });
-		// var bottomBoundary = world.createEntity(boundaryTemplate, { height: 2, y: 50.5 });
-
-		// var width = GAME_SIZE.width / GAME_SCALE;
-		// var height = GAME_SIZE.height / GAME_SCALE;
-		// console.log(width);
-
-		// var wall = world.createEntity({
-		// 	name: 'wall',
-		// 	shape: 'polygon',
-		// 	type: 'static',
-		// 	color: 'transparent',
-		// 	x:0, 
-		// 	y:0,
-		// 	points: [{ x: 0, y: 0 }, 
-		// 			{ x: 0, y: height },
-		// 			{ x: width, y: height }, 
-		// 			{ x: width, y: 0 } ]
-		// });
-	}
-
-	var createLineCarry = function(config){
-		var lineTemplate = {
-			name: 'line',
+	function createLifter(lifterConfig){
+		var lifter = world.createEntity({
+			name: 'lifter',
+			shape: 'square',
 			type: 'static',
-			x: 0,
-			y: 0,
+			width: lifterConfig.width,
+			height: lifterConfig.height,
+			x: lifterConfig.x + lifterConfig.width/2,
+			y: lifterConfig.y + lifterConfig.height/2,
+			$initialY: lifterConfig.y + lifterConfig.height/2,
+			$incUp: lifterConfig.incUp,
+			$incDown: lifterConfig.incDown,
+			$cur: 1,
+			color: 'green',
+			onTick: function(){
+				var pos = this.position();
+				this.position({x: pos.x, y: pos.y + this.$cur});
 
-		};
-	}
+				if(pos.y > this.$initialY + this.$incUp){
+					this.$cur *= -1;
+				}
+				else if(pos.y < this.$initialY + this.$incDown){
+					this.$cur *= -1;
+				}
+			}
+		});
 
+		lifter.setVelocity('move', 0.5, 180)
+	};
 
-	var createNewLevel = function(level){
-		startTime = new Date().getTime();
-
-		createBorders();
-
-		var ballTemplate = {
+	function createBall(ballConfig){
+		world.createEntity({
 			name: 'ball',
 			shape: 'circle',
-			color: 'white',
-			radius: 0.5
-		};
-
-		var ball = world.createEntity(ballTemplate);
-
-		var ballHolder = {
-			name: 'ballHolder', 
-			shape: 'square',
-			color: 'green',
-			type: 'static',
-			x: ball.position().x, 
-			y: ball.position().y + ballTemplate.radius + 0.1,
-			rotation: 45,
-			width: ballTemplate.radius,
-			height: 0.1
-		};
-
-		var lineTemplate = {
-			name: 'line',
-			shape: 'polygon', 
-			color: 'darkblue',
-			type: 'static',
-			borderWidth: 5,
-			$isInsideNow: false,
-			onMousedown: function(){
-				//this.roation(this.rotation() + 5);
+			color: 'lightblue',
+			radius: ballConfig.radius,
+			onkeydown: function(event){
+				//var direction = 90 ? event.keyCode === KEYCODES_ARROWS_RIGHT : 270;
+				//this.applyImpulse(50, direction);
 			}
+		});
+	};
+
+	function createExit(exitConfig){
+		var mwidth = 2;
+		var mheight = 2;
+		world.createEntity({
+			name: 'ball',
+			shape: 'square',
+			width: mwidth,
+			height: mheight,
+			x: exitConfig.x + mwidth / 2,
+			y: exitConfig.y + mheight / 2,
+			onImpact: function(entity){
+				if(entity.name == 'ball'){
+					moveToNextLevel();
+				}
+			}
+		});
+
+	};
+
+	function createNewLevel(level){
+		needRestartLevel = false;
+		needMoveToNextLevel = false;
+
+		createBall(level.ball);
+
+		createExit(level.exit);
+
+		var lifters = level.lifters;
+		for(var i=0; i<lifters.length; i++){
+			createLifter(lifters[i]);
 		}
 
-		var ballHolder = world.createEntity(ballHolder);
-
-		var newLineTemplate = {
-			name: 'bomba',
-			shape: 'square',
-			x:6, 
-			y:5,
-			width: 3,
-			height: 0.5,
-			onTick: function(){
-				//this.rotation(this.rotation() + this.$add);
-			}
-		};
-
-		var k = world.createEntity(newLineTemplate);
-		
-		var bomba = function(){
-			k.applyImpulse(50, 0);
-			setTimeout(bomba, 1000);
-		}
-		setTimeout(bomba, 1000);
-		//k.setForce('komalo', 50, 1);
-
-		world.createEntity(newLineTemplate, {x: 5, y: 9, rotation: Math.random() * 360, $add: -10});
-		world.createEntity(newLineTemplate, {x: 10, y: 10, rotation: Math.random() * 360, $add: +10});
-
-		var start = {x:0, y:0};
-		// world.onMousedown(function(event, mouseInfo){
-		// 	var m = mouseInfo;//world.calculateWorldPositionFromPointer(event);
-		// 	start.x = m.x;
-		// 	start.y = m.y;
-		// });
-			
 		world.pause();
-
-
-		// setTimeout(function(){
-		// 	world.pause();
-		// 	console.log('a');
-		// 	ballHolder.destroy();
-		// }, 1000);
-
-		//world.camera({x:0, y:0});
-		// world.onMouseup(function(event, mouseInfo){
-		// 	var m = mouseInfo;
-		// 	//var o = world.calculateWorldPositionFromPointer(event);
-		// 	var width  = Math.abs(start.x - m.x);
-		// 	var height = Math.abs(start.y - m.y);
-
-		// 	console.log(start);
-		// 	console.log(m);
-
-		// 	// world.createEntity({
-		// 	// 	name:'komalo',
-		// 	// 	type:'square',
-		// 	// 	x: m.x,
-		// 	// 	y: m.y
-		// 	// });
-
-		// 	world.createEntity(lineTemplate, { 
-		// 		x: 0,
-		// 		y: 0,
-		// 		points :[{
-		// 			x: start.x,
-		// 			y: start.y
-		// 		},{
-		// 			x: m.x,
-		// 			y: m.y}]
-		// 		});
-		// });
-		
-		//  var komalo = {x:1, y:1};
-
-		// world.onMousemove(function(event, mouseInfo){
-		// 	//var m = mouseInfo;
-		// 	komalo.x = event.clientX;
-		// 	komalo.y = event.clientY;
-		// 	console.log(komalo);
-		// });
-
-		// world.onRender(function(cx){
-		// 	cx.fillStyle = "black";
-		// 	cx.fillRect(komalo.x, komalo.y, 10, 10);
-
-		// });
 	};
 
 	function restartLevel(){
@@ -251,10 +167,9 @@ var GameController = (function(){
 		world.pause();
 	}
 
-	world.onRender(function(context){
+	function drawScore(context){
 		context.fillStyle = "black";
 		context.font="13px Verdana";
-		var trials = getFailTrials();
 		var elapsedTime = getElapsedTime();
 
 		context.fillStyle="darkgrey";
@@ -262,8 +177,7 @@ var GameController = (function(){
 		var infoX = 5;
 		var infoY = 14;
 		context.fillText("Time Elapsed: " + elapsedTime, infoX, infoY);
-		context.fillText("Failed Trials: " + trials, infoX, infoY + 20);
-		context.fillText("Level " + nextLevelIndex + ": " + level.name, infoX, infoY + 40);
+		context.fillText("Level " + nextLevelIndex + ": " + levels[nextLevelIndex-1].name, infoX, infoY + 20);
 
 		if(GAME_END){
 			context.fillStyle="rgba(0,0,0,0.7)";
@@ -275,16 +189,32 @@ var GameController = (function(){
 			var y = GAME_SIZE.height/2 - 60;
 			context.fillText("Congratualations, you have successfully", x, y);
 			context.fillText('finished all the levels within time: ' + elapsedTime, x, y + 30);
-			context.fillText('and fail trials: ' + trials, x, y + 60);
 		}
-	});
+	}
+
+	
+	function initWorldFuncs(){
+		world.onTick(function(){
+			if(needRestartLevel){
+				setTimeout(restartLevel(), 10);
+			}
+
+			if(needMoveToNextLevel){
+				setTimeout(moveToNextLevel(), 10);
+			}
+		});
+
+		world.onRender(drawScore);
+	}
+
 
 	moveToNextLevel();
-	
-	world.pause();
+
+	initWorldFuncs();
 
 	return {
 		startGame: function(){
+			startTime = new Date().getTime();
 			world.pause();
 		}
 	};
